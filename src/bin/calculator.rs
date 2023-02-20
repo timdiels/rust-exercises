7546f0e2fa21200aa7a14dddd4e8db6d8cd30538
Tim Diels <tim.diels@ontoforce.com>
2023-02-20 18:04:27 +0100

    Use traits and handle divide by zero

--- src/bin/calculator.rs
+++ src/bin/calculator.rs
@@ -2,6 +2,9 @@ use std::io::{self, BufRead, Write};
 use std::iter::Iterator;
 use std::process;
 
+#[macro_use]
+extern crate is_close;
+
 fn main() {
     let mut stack: Vec<f64> = Vec::new();
     let command_results = io::stdin().lock().lines().map(|line_result| {
@@ -16,16 +19,17 @@ fn main() {
         }
         print_cli_head();
     }
+    println!("Ctrl-d? Trying to be fancy huh?");
 }
 
 fn parse_line(line: String) -> Result<Command, String> {
     match line.as_str() {
         "quit" | "q" => Ok(Command::Quit),
         "dump" | "d" => Ok(Command::Dump),
-        "+" => Ok(Command::Add),
-        "-" => Ok(Command::Subtract),
-        "*" => Ok(Command::Multiply),
-        "/" => Ok(Command::Divide),
+        "+" => Ok(Command::PerformBinaryCalculation(Box::new(Add))),
+        "-" => Ok(Command::PerformBinaryCalculation(Box::new(Subtract))),
+        "*" => Ok(Command::PerformBinaryCalculation(Box::new(Multiply))),
+        "/" => Ok(Command::PerformBinaryCalculation(Box::new(Divide))),
         _ => match line.parse() {
             Ok(number) => Ok(Command::PushNumber(number)),
             Err(_) => Err(format!("Unknown command or invalid float: {line}")),
@@ -44,33 +48,7 @@ fn handle_command(stack: &mut Vec<f64>, command: Command) -> Result<(), String> 
             Ok(())
         }
         Command::PushNumber(number) => Ok(stack.push(number)),
-        Command::Add => {
-            // I miss inheritance, maybe traits will help
-            apply_binary_operator(stack, |a, b| Ok(a + b))
-        }
-        Command::Subtract => apply_binary_operator(stack, |a, b| Ok(a - b)),
-        Command::Multiply => apply_binary_operator(stack, |a, b| Ok(a * b)),
-        Command::Divide => {
-            // TODO handle divide by zero
-            apply_binary_operator(stack, |a, b| Ok(a / b))
-        }
-    }
-}
-
-fn apply_binary_operator<F: Fn(f64, f64) -> Result<f64, String>>(
-    stack: &mut Vec<f64>,
-    calculate: F,
-) -> Result<(), String> {
-    match stack.len() {
-        0 => Err("The stack is empty!".to_string()),
-        1 => Err("Only 1 number on the stack!".to_string()),
-        _ => {
-            let number2 = stack.pop().expect("Got at least 2 numbers");
-            let number1 = stack.pop().expect("Still got at least 1 number");
-            stack.push(calculate(number1, number2)?);
-            print_stack(&stack);
-            Ok(())
-        }
+        Command::PerformBinaryCalculation(calculation) => calculation.apply_to_stack(stack),
     }
 }
 
@@ -88,9 +66,57 @@ fn print_stack(stack: &Vec<f64>) -> () {
 enum Command {
     Quit,
     Dump,
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
     PushNumber(f64),
+    PerformBinaryCalculation(Box<dyn BinaryCalculation>),
+}
+
+trait BinaryCalculation {
+    fn calculate(&self, number1: f64, number2: f64) -> Result<f64, String>;
+
+    fn apply_to_stack(&self, stack: &mut Vec<f64>) -> Result<(), String> {
+        match stack.len() {
+            0 => Err("The stack is empty!".to_string()),
+            1 => Err("Only 1 number on the stack!".to_string()),
+            _ => {
+                let numbers = &stack[stack.len() - 2..];
+                let number = self.calculate(numbers[0], numbers[1])?;
+                stack.truncate(stack.len() - 2);
+                stack.push(number);
+                print_stack(&stack);
+                Ok(())
+            }
+        }
+    }
+}
+
+struct Add;
+impl BinaryCalculation for Add {
+    fn calculate(&self, number1: f64, number2: f64) -> Result<f64, String> {
+        Ok(number1 + number2)
+    }
+}
+
+struct Subtract;
+impl BinaryCalculation for Subtract {
+    fn calculate(&self, number1: f64, number2: f64) -> Result<f64, String> {
+        Ok(number1 - number2)
+    }
+}
+
+struct Multiply;
+impl BinaryCalculation for Multiply {
+    fn calculate(&self, number1: f64, number2: f64) -> Result<f64, String> {
+        Ok(number1 * number2)
+    }
+}
+
+struct Divide;
+impl BinaryCalculation for Divide {
+    fn calculate(&self, number1: f64, number2: f64) -> Result<f64, String> {
+        if is_close!(number2, 0.0) {
+            Err("Cannot divide by zero".to_string())
+        } else {
+            Ok(number1 / number2)
+        }
+    }
 }
